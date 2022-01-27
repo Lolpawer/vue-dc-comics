b85f248c23c2ea667f8fa3edcf91d239226111ce
Lolpawer <dadu.petrosino@gmail.com>
2022-01-27 17:28:44 +0100

    header + moved images

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
   <div id="app">
+    <header-box>
+    </header-box>
   </div>
 </template>
 
 <script>
+import HeaderBox from './components/HeaderBox.vue'
+
 export default {
   name: 'App',
   components: {
+    HeaderBox
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="scss" scoped>
+@import './style/main.scss'
 </style>
---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,77 @@
+<template>
+    <header>
+        <div class="container">
+            <div class="logo">
+                <img src="@/assets/dc-logo.png" />
+            </div>
+            <nav>
+                <ul>
+                    <li>CHARACTERS</li>
+                    <li>COMICS</li>
+                    <li>MOVIES</li>
+                    <li>TV</li>
+                    <li>GAMES</li>
+                    <li>COLLECTIBLES</li>
+                    <li>VIDEOS</li>
+                    <li>FANS</li>
+                    <li>NEWS</li>
+                    <li>SHOP</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+    header {
+        
+        .container {
+            width: 1200px;
+            height: 100px;
+            display: flex;
+            margin: 0 auto;
+            justify-content: space-between;
+            align-items: center;
+    
+            .logo {
+                width: 75px;
+                height: 75px;
+               
+                img {
+                    width: 100%;
+                    height: 100%;
+                }
+            }
+
+            nav {
+                height: 100%;
+
+                ul{
+                    height: 100%;
+                    list-style: none;
+                    display: flex;
+                    margin: 0; //per qualche motivo si da un margin di 16px da solo?
+                
+                    li{
+                        margin: 0 10px;
+                        font-size: 12px;
+                        font-family: Arial, Helvetica, sans-serif;
+                        font-weight: bold;
+                        color: #464646;
+                        line-height: 100px;
+
+                        &:hover{
+                            color: #0282f9;
+                            border-bottom: 5px solid #0282f9;
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+</style>
