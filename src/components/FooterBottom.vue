ff79c63741a4747fd5c9efc52f7c59554c202ddb
Lolpawer <dadu.petrosino@gmail.com>
2022-01-27 18:57:40 +0100

    footer done

---
+++ src/components/FooterBottom.vue
@@ -0,0 +1,60 @@
+<template>
+    <div>
+        <div class="container">
+            <div class="button">
+                SIGN-UP NOW!
+            </div>
+            <div class="logos">
+                <span>FOLLOW US</span>
+                <img src="@/assets/footer-facebook.png" />
+                <img src="@/assets/footer-twitter.png" />
+                <img src="@/assets/footer-youtube.png" />
+                <img src="@/assets/footer-pinterest.png" />
+                <img src="@/assets/footer-periscope.png" />
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+</script>
+
+<style lang="scss" scoped>
+
+    div {
+        background-color: #303030;
+
+        .container {
+            width: 1200px;
+            margin: 0 auto;
+            height: 100px;
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+            font-family: Arial, Helvetica, sans-serif;
+            font-weight: bold;
+
+            .button {
+                border: 2px solid #0282f9;
+                padding: 15px 12px;
+                color: #fff;
+            }
+
+            .logos {
+
+                span{
+                    color: #0282f9;
+                    margin: 0 10px;
+                    font-size: 20px;
+                    vertical-align: middle;
+                }
+
+                img {
+                    vertical-align: middle;
+                    margin: 0 10px;
+                }
+            }
+        }
+    }
+
+</style>
--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -2,17 +2,20 @@
     <div>
         <footer-top></footer-top>
         <footer-middle></footer-middle>
+        <footer-bottom></footer-bottom>
    </div>
 </template>
 
 <script>
 import FooterTop from './FooterTop.vue'
 import FooterMiddle from './FooterMiddle.vue'
+import FooterBottom from './FooterBottom.vue'
 
 export default {
     components: {
         FooterTop,
-        FooterMiddle
+        FooterMiddle,
+        FooterBottom
     }
 }
 
